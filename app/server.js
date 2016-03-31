682e9a122eade8662c1333498e7a005ce539d125
Christopher Pies <cpies@deseretdigital.com>
2016-03-31 12:07:00 -0600

    Adds eslint [#116251939]

--- app/server.js
+++ app/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +7,7 @@ const api = require('../lib/api');
 
 app.use('/api/v1', api);
 
-app.listen(port, function() {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+/* eslint-enable no-console */
