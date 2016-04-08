f898dc9d60fcc66ffc53a07f4469f1844aceb1be
sdickson <sdickson@deseretdigital.com>
2016-04-08 09:12:00 -0600

    [#116249325] embadable microservice now has an option to pass the body with urls and get the rendered outputs in response  (POST to /parse-embeds with a body field)

--- app/server.js
+++ app/server.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-console */
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const api = require('../lib/api');
+
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1', api);
 
