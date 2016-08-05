3c485030d324df3e4b0a95bb55629aca13f0c0bd
Matt Montgomery <mmontgomery@deseretdigital.com>
2016-08-05 15:37:46 -0600

    Allow cross-origin-resource-sharing

--- app/server.js
+++ app/server.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const api = require('../lib/api');
 
+app.use(cors);
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
