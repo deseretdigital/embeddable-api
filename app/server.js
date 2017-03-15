3d1158753811b839ce8a4daf3e966d9ae1611ce1
Matt Montgomery <mmontgomery@deseretdigital.com>
2017-03-15 11:58:42 -0600

    Send mountpath

--- app/server.js
+++ app/server.js
@@ -9,6 +9,7 @@ const api = require('../lib/api');
 app.all('/*', (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('x-Mount-Path', app.mountpath);
     next();
 });
 
